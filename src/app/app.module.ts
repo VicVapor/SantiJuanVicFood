1175ffe7de22f0470560f3962db7bbcf3b45a0b9
VicVapor <victorj.gutierrezp@konradlorenz.edu.co>
2023-05-14 19:59:33 -0500

    Push
    Push para Santiago

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 
 // Components
@@ -8,7 +9,6 @@ import { AppComponent } from './app.component';
 import { CreateUserComponent } from './components/create-user/create-user.component';
 import { ListUsersComponent } from './components/list-users/list-users.component';
 import { LoginComponent } from './components/login/login.component';
-import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
--- src/app/components/create-user/create-user.component.ts
+++ src/app/components/create-user/create-user.component.ts
@@ -1,10 +1,28 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-create-user',
   templateUrl: './create-user.component.html',
-  styleUrls: ['./create-user.component.css']
+  styleUrls: ['./create-user.component.css'],
 })
 export class CreateUserComponent {
+  
+  userForm: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.userForm = this.fb.group({
+      name: ['', Validators.required],
+      lastname: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+      petName: ['', Validators.required],
+      mondayFeedingTime: ['', Validators.required],
+      tuesdayFeedingTime: ['', Validators.required],
+      wednesdayFeedingTime: ['', Validators.required],
+      thursdayFeedingTime: ['', Validators.required],
+      fridayFeedingTime: ['', Validators.required],
+    });
+  }
 
 }
