20e604a0259e42a971ddd1fbfc46dd54ea1cb401
VicVapor <victorj.gutierrezp@konradlorenz.edu.co>
2023-05-18 22:41:13 -0500

    Cámara
    Se arregla la detección por cámara

--- src/app/components/detect-object/detect-object.component.ts
+++ src/app/components/detect-object/detect-object.component.ts
@@ -1,17 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import './detect-object.js';
-import 'ml5';
-
 
 @Component({
   selector: 'app-detect-object',
   templateUrl: './detect-object.component.html',
   styleUrls: ['./detect-object.component.css'],
 })
-export class DetectObjectComponent implements OnInit{
-  ngOnInit() {
-    const toggleDetectingButton = document.getElementById('detectionAction');
-    toggleDetectingButton.addEventListener('click', toggleDetecting);
-    
-  }
+export class DetectObjectComponent implements OnInit {
+  ngOnInit() {}
 }
